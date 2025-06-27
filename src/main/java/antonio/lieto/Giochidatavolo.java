8b28cf07429e1a9d27ff27cdd384934049d8561c
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-06-27 10:59:16 +0200

    implemento giochi da tavolo

--- src/main/java/antonio/lieto/Giochidatavolo.java
+++ src/main/java/antonio/lieto/Giochidatavolo.java
@@ -1,6 +1,42 @@
 package antonio.lieto;
 
 public class Giochidatavolo extends Giochi{
-    
+    public int numeroGiocatori;
+    public int durataMedia;
 
+
+    public Giochidatavolo(int id, String titolo, int anno, int prezzo, int numeroGiocatori, int durataMedia) throws IllegalAccessException {
+        super(id, titolo, anno, prezzo);
+
+        if(numeroGiocatori < 2 || numeroGiocatori >10){
+            throw new IllegalAccessException("i giocatori devono essere almeno 2 e massimo 10");
+        }
+
+        this.numeroGiocatori = numeroGiocatori;
+        this.durataMedia = durataMedia;
+    }
+
+        public int getNumeroGiocatori() {
+            return numeroGiocatori;
+        }
+
+        public void setNumeroGiocatori(int numeroGiocatori) {
+            this.numeroGiocatori = numeroGiocatori;
+        }
+
+        public int getDurataMedia() {
+            return durataMedia;
+        }
+
+        public void setDurataMedia(int durataMedia) {
+            this.durataMedia = durataMedia;
+        }
+
+    @Override
+    public String toString() {
+        return "Giochidatavolo{" +
+                "numeroGiocatori=" + numeroGiocatori +
+                ", durataMedia=" + durataMedia +
+                '}';
+    }
 }
